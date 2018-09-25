3b0de60fb201a3c3b812d9a1466d9af03faf5712
tagirmi <tagirmi@gmail.com>
2018-09-25 21:04:53 +0300

    улучшения, и добавил комментарии doxygen

--- print_ip.h
+++ print_ip.h
@@ -38,6 +38,9 @@ constexpr bool is_container_v = is_container<
 template<typename T>
 constexpr bool is_integral_v = std::is_integral<T>::value;
 
+template<typename T, typename U>
+constexpr bool is_same_v = std::is_same<T, U>::value;
+
 static_assert(is_container_v<const std::vector<int>&>, "is_container<const std::vector<T>&>");
 static_assert(is_container_v<const std::vector<int>>, "is_container<const std::vector<T>>");
 static_assert(is_container_v<std::vector<int>>, "is_container<std::vector<T>>");
@@ -57,22 +60,8 @@ void container_print_helper(std::ostream& stream, const T& container)
   stream << std::endl;
 }
 
-}
-
-template<typename T, typename std::enable_if_t<details::is_container_v<T>, int> = 0>
-void print_ip(std::ostream& stream, const T& container)
-{
-  details::container_print_helper(stream, container);
-}
-
 template<typename T>
-void print_ip(std::ostream& stream, const std::basic_string<T>& string)
-{
-  stream << string << std::endl;
-}
-
-template<typename T, typename std::enable_if_t<details::is_integral_v<T>, int> = 0>
-void print_ip(std::ostream& stream, const T& value)
+void integral_print_helper(std::ostream& stream, const T& value)
 {
   std::vector<int> vector;
   vector.reserve(sizeof(T));
@@ -83,7 +72,54 @@ void print_ip(std::ostream& stream, const T& value)
   std::copy(begin, end, std::back_inserter(vector));
   std::reverse(vector.begin(), vector.end());
 
-  details::container_print_helper(stream, vector);
+  container_print_helper(stream, vector);
 }
 
+} // details
+
+/**
+ * @brief Специализация шаблона функции для печати IP адреса для значений интрегральных типов.
+ * Выводит побайтово начиная со старшего с символом '.' в качестве разделителя.
+ *
+ * @tparam T Интегральный тип.
+ * @param stream Поток вывода.
+ * @param value Значение интегрального типа.
+ */
+template<typename T>
+std::enable_if_t<details::is_integral_v<T>, void>
+print_ip(std::ostream& stream, const T& value)
+{
+  details::integral_print_helper(stream, value);
 }
+
+/**
+ * @brief Специализация шаблона функции для печати IP адреса для строки.
+ * Строку выводит как есть.
+ *
+ * @tparam T std::string.
+ * @param stream Поток вывода.
+ * @param string Строка.
+ */
+template<typename T>
+std::enable_if_t<details::is_same_v<T, std::string>, void>
+print_ip(std::ostream& stream, const T& string)
+{
+  stream << string << std::endl;
+}
+
+/**
+ * @brief Специализация шаблона функции для печати IP адреса для контейнеров std::vector и std::list.
+ * Выводится содержимое контейнера поэлементно и разделяется '.'.
+ *
+ * @tparam T контейнер std::vector или std::list.
+ * @param stream Поток вывода.
+ * @param container Контейнер.
+ */
+template<typename T>
+std::enable_if_t<details::is_container_v<T>, void>
+print_ip(std::ostream& stream, const T& container)
+{
+  details::container_print_helper(stream, container);
+}
+
+} // hw04
